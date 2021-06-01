30a1d2982354505f87a685c5f9a7c292689c4fb4
rangademetal <stro3andr3i@gmail.com>
2021-06-01 11:09:05 +0300

    Added scroll down, hr research and description data extracting

--- Linkedin.py
+++ Linkedin.py
@@ -14,4 +14,17 @@ class Linkedin:
 		button = self.driver.find_element_by_xpath('/html/body/main/section[1]/div[2]/form/button').click()
 	def hr(self):
 		self.driver.get('https://www.linkedin.com/jobs/search/?keywords=hr')
-		
+	def scroll_down(self):
+		target = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[1]/div/div') 
+		self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', target)
+	def data_mining(self):
+		items = self.driver.find_elements_by_class_name('artdeco-entity-lockup__title')
+		jobs = [jobs.get_attribute('id') for jobs in items]
+		for job in jobs:
+			item = self.driver.find_element_by_id(job)
+			item.click()
+			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[2]/article/div/div[1]/span').text
+			print(dummy)
+
+
+
