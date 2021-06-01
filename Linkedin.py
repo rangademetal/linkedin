0c46cb41e0ec395e7e85e3a3e86e18562c987bb7
rangademetal <stro3andr3i@gmail.com>
2021-06-01 16:55:42 +0300

    update]

--- Linkedin.py
+++ Linkedin.py
@@ -1,7 +1,10 @@
+import random
+import xlsxwriter
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from Secret import USERNAME, PASSWORD
-
+  
 class Linkedin:
 	def __init__(self):
 		self.driver = webdriver.Firefox(executable_path='C:\\Users\\NaiTuTreaba\\PycharmProjects\\webscrapper\\linkedin\\geckodriver.exe')
@@ -17,41 +20,48 @@ class Linkedin:
 	def scroll_down(self):
 		target = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[1]/div/div') 
 		self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', target)
+	def scroll_up(self):
+		self.driver.execute_script("window.scrollTo(0, 220)")
 	def next_page(self):
 		dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[1]/div/div/section/div/ul/li[10]/button').text	
 		return 25*(int(dummy)-1)
 		
 	def data_mining(self):
+		rnd = random.randint(10000,99999)
+		workbook = xlsxwriter.Workbook(f'{rnd}.xlsx')
+		worksheet = workbook.add_worksheet()
+
+		worksheet.write(f'F1', 'Job Description')
+		worksheet.write(f'B1', 'Job Location')
+		worksheet.write(f'C1', 'Job level')
+		worksheet.write(f'D1', 'Job Function')
+		worksheet.write(f'E1', 'Company')
+		worksheet.write(f'A1', 'Applicants')
+		res = []
 		items = self.driver.find_elements_by_class_name('artdeco-entity-lockup__title')
 		jobs = [jobs.get_attribute('id') for jobs in items]
 		for job in jobs:
 			item = self.driver.find_element_by_id(job)
 			item.click()
-			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/div/div/div[2]/div[1]/div[1]/a/h2').text
-			# Job description
-			print(dummy) 
-			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[2]/article/div/div[1]/span').text
-			# job function
-			print(dummy)
-			dummy = self.driver.find_element_by_xpath(f'//*[@id="{job}"]').text
-			# company
-			print(dummy)
-			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/div/div/div[2]/div[1]/div[1]/div/span[3]').text
-			# Location
-			print(dummy) 
-			# level
-			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/ul/li[1]/div/ul/li[1]/span').text
-			print(dummy)
-			# number of aplications on the job
-			dummy = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/ul/li[1]/div/ul/li[2]/span').text
-			print(dummy)
+			dict = []
+			# mining
+			company = self.driver.find_element_by_xpath(f'//*[@id="{job}"]').text
+			job_function = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[2]/article/div/div[1]/span').text
+			location = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/div/div/div[2]/div[1]/div[1]/div/span[3]').text
+			level = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/ul/li[1]/div/ul/li[1]/span').text
+			aplicants = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/ul/li[1]/div/ul/li[2]/span').text
+			job_description = self.driver.find_element_by_xpath('/html/body/div[8]/div[3]/div[3]/div/div/section[2]/div/div/div[1]/div/div[1]/div/div/div[2]/div[1]/div[1]/a/h2').text
+			
+			dict.append(company)
+			dict.append(job_function)
+			dict.append(location)
+			dict.append(level)
+			dict.append(aplicants)
+			dict.append(job_description)
+
+			res.append(dict)
+		return res
+
 	def next(self, total_jobs, jobs):
 		if total_jobs != jobs:
 			self.driver.get(f'https://www.linkedin.com/jobs/search/?keywords=hr&start={jobs}')
-
-
-
-			
-
-
-
--- exl.py
+++ exl.py
@@ -1,26 +1,17 @@
-
-# import xlsxwriter module
 import random
 import xlsxwriter
   
-# Workbook() takes one, non-optional, argument 
-# which is the filename that we want to create.
 count = random.randint(10000,99999)
 workbook = xlsxwriter.Workbook(f'{count}.xlsx')
   
-# The workbook object is then used to add new 
-# worksheet via the add_worksheet() method.
-worksheet = workbook.add_worksheet()
-  
-# Use the worksheet object to write
-# data via the write() method.
-worksheet.write('A1', 'Job Description')
-worksheet.write('B1', 'Job Location')
-worksheet.write('C1', 'Job level')
-worksheet.write('D1', 'Job Function')
-worksheet.write('E1', 'Company')
-worksheet.write('F1', 'Applicants')
 
-# Finally, close the Excel file
-# via the close() method.
+worksheet = workbook.add_worksheet()
+count = 1 
+worksheet.write(f'F1', 'Job Description')
+worksheet.write(f'B1', 'Job Location')
+worksheet.write(f'C1', 'Job level')
+worksheet.write(f'D1', 'Job Function')
+worksheet.write(f'E1', 'Company')
+worksheet.write(f'A1', 'Applicants')
+
 workbook.close()
